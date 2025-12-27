d4d0144d42e3fbd13a0229dd23a971fe3c473b6c
sreeharipradeep <83856203+sreeharipradeep@users.noreply.github.com>
2025-12-27 10:16:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
+# Initialize Pyrogram Client
 app = Client(
     "MovieFilterBot",
     api_id=API_ID,
@@ -8,8 +9,11 @@ app = Client(
     bot_token=BOT_TOKEN
 )
 
+# Import command handlers
 import start
 import filter
 import broadcast
 import help
+
+# Run the bot
 app.run()
