395140256a061a8ba9a8525f88f29444a1e83ba1
sreeharipradeep <83856203+sreeharipradeep@users.noreply.github.com>
2025-12-27 10:08:48 +0530

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -1,10 +1,12 @@
 from pyrogram import Client, filters
 from config import ADMINS
 
+# /broadcast command for admins only
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
 async def broadcast(client, message):
     if not message.reply_to_message:
-        return await message.reply("Reply to a message")
+        return await message.reply("Reply to a message to broadcast")
 
+    # Copy the replied message to the same chat
     await message.reply_to_message.copy(message.chat.id)
     await message.reply("✅ Broadcast Done")
