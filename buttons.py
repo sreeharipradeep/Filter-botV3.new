3b9e6df5c594a2d20b9608540894d87e221c2c04
sreeharipradeep <83856203+sreeharipradeep@users.noreply.github.com>
2025-12-27 08:57:19 +0530

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -2,13 +2,13 @@ from pyrogram.types import InlineKeyboardButton
 
 start_buttons = [
     [
-        InlineKeyboardButton("👥 Group", url="https://t.me/yourgroup"),
-        InlineKeyboardButton("📢 Channel", url="https://t.me/yourchannel")
+        InlineKeyboardButton("👥 Group", url="https://t.me/trixel_movies"),
+        InlineKeyboardButton("📢 Channel", url="https://t.me/+F2nvRjDLzhIzMjg9")
     ],
     [
         InlineKeyboardButton(
             "➕ Add Me To Your Group",
-            url="https://t.me/YourBotUsername?startgroup=true"
+            url="https://t.me/@tony_stark_v3_bot?startgroup=true"
         )
     ]
 ]
